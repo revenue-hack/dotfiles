f92e2b421e9121ab5a25d22d947371745b365c99
revenue-hack <K.odeveloper10@gmail.com>
2020-03-31 17:53:33 +0900

    feat: git config is current

--- shell/setup-git.sh
+++ shell/setup-git.sh
@@ -2,6 +2,7 @@
 if type "git" > /dev/null 2>&1; then
   git config --global user.name "revenue-hack"
   git config --global user.email K.odeveloper10@gmail.com
+  git config --global push.default current
   echo "success git config"
 else
   echo "git command not exist"
