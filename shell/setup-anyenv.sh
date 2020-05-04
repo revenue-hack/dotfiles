55cfa1669ce2bce1234148403090897dbf1c7357
revenue-hack <K.odeveloper10@gmail.com>
2020-05-04 09:13:33 +0900

    refactor: migrate install shell

--- shell/setup-anyenv.sh
+++ shell/setup-anyenv.sh
@@ -29,23 +29,7 @@ if type "nodenv" > /dev/null 2>&1 ; then
   nodenv global 12.10.0
 
   if type "npm" > /dev/null 2>&1 ; then
-    # TypescriptのLSP
-    npm install -g typescript typescript-language-server
-    # PHPのLSP
-    npm -g install intelephense
-    # VueのLSP
-    npm i -g vls
-    npm i -g eslint
-    npm i -g eslint-loader
-    npm i -g eslint-plugin-vue
-    # Dockerfile LSP
-    npm install -g dockerfile-language-server-nodejs
-    # Bash LSP
-    npm i -g bash-language-server
-    # Vim LSP
-    npm install -g vim-language-server
-    # SQL LSP
-    npm i -g sql-language-server
+    ./install-npm.sh
   else
     echo "npm not found"
   fi
