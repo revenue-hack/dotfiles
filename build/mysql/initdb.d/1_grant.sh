8f6342401a91596dc8372bef54672e7fe61ad544
masahiro.sakuraba <masahiro.sakuraba@kaonavi.jp>
2023-01-17 16:30:19 +0900

    add env MYSQL_DATABASE

--- build/mysql/initdb.d/1_grant.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-MYSQL="mysql -uroot -p${MYSQL_ROOT_PASSWORD}"
-
-echo "\
-GRANT ALL ON *.* TO 'root'@'%';
-FLUSH PRIVILEGES;
-" | ${MYSQL}
--- build/mysql/initdb.d/2_create_database.sql
+++
@@ -1,2 +0,0 @@
--- FIXME: このファイルは後で不要になるはずなので消す
-CREATE DATABASE `local` DEFAULT CHARACTER SET utf8mb4;
