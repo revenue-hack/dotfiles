a5c2a806a4d7152083a31b8754539608aea4005a
masahiro.sakuraba <masahiro.sakuraba@kaonavi.jp>
2023-02-03 19:30:28 +0900

    マイグレーション+環境構築周り

--- README.md
+++ README.md
@@ -2,5 +2,34 @@
 
 LMS(Leaning Management System)のAPI等を提供するためのリポジトリ
 
-DHAが豊富な鰯を食べて、LMSでいっぱい勉強すれば頭が良くなるはずです。
+## 環境構築手順
+
+### 前提条件
+
+- shironegiの環境構築が終わっていること
+
+### 1. .envの作成
+
+```sh
+$ cp .env.example .env
+```
+
+`.env` の以下のキーにshironegiの構築時に設定したDB名と同じ値（通常は社員番号）を設定。
+
+```
+DB_DATABASE=[社員番号]
+MYSQL_DATABASE=[社員番号]
+```
+
+### 2. docker構築
+
+```sh
+$ docker compose up -d --build
+```
+
+### 3. DBのマイグレーション
+
+```sh
+$ docker compose exec go make migrate
+```
 
---
+++ build/migrate/1_initialize_ddl.down.sql
@@ -0,0 +1,24 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE `categories`;
+DROP TABLE `courses`;
+DROP TABLE `e_learnings`;
+DROP TABLE `group_trainings`;
+DROP TABLE `group_training_schedules`;
+DROP TABLE `movies`;
+DROP TABLE `files`;
+DROP TABLE `urls`;
+DROP TABLE `target_members`;
+DROP TABLE `target_member_movie_watch_statuses`;
+DROP TABLE `target_member_file_watch_statuses`;
+DROP TABLE `target_member_url_watch_statuses`;
+DROP TABLE `forms`;
+DROP TABLE `form_parts`;
+DROP TABLE `form_part_choices`;
+DROP TABLE `form_answers`;
+DROP TABLE `form_answer_choices`;
+DROP TABLE `reminders`;
+DROP TABLE `reminder_schedules`;
+DROP TABLE `reminder_schedule_send_histories`;
+
+SET FOREIGN_KEY_CHECKS = 1;
