a0049015e3ba10a6695992c38844891b48d688ea
masahiro.sakuraba <masahiro.sakuraba@kaonavi.jp>
2023-02-09 11:29:40 +0900

    テーブル構成の修正

--- build/migrate/2_initialize_sample_dml.up.sql
+++ build/migrate/2_initialize_sample_dml.up.sql
@@ -4,13 +4,13 @@ set @user_id = 1;
 INSERT INTO `categories` (`id`, `name`, `display_order`, `created_by`, `updated_by`)
 VALUES
     (1, '新人研修', 1, @user_id, @user_id),
-    (2, 'セキュリティ', 1, @user_id, @user_id),
-    (3, 'キャリア支援', 1, @user_id, @user_id),
-    (4, '全社員必須', 1, @user_id, @user_id);
+    (2, 'セキュリティ', 2, @user_id, @user_id),
+    (3, 'キャリア支援', 3, @user_id, @user_id),
+    (4, '全社員必須', 4, @user_id, @user_id);
 
-INSERT INTO `courses` (`id`, `name`, `description`, `content_type`, `is_required`, `category_id`, `created_by`, `updated_by`)
+INSERT INTO `courses` (`id`, `course_type`, `title`, `description`, `is_required`, `category_id`, `status`, `created_by`, `updated_by`)
 VALUES
-    (1, 'ビジネスマナーの基本', '', 1, 1, 1, @user_id, @user_id),
-    (2, 'ロジカルシンキング研修', '', 1, 1, 1, @user_id, @user_id),
-    (3, '情報セキュリティ研修', '', 1, 0, 2, @user_id, @user_id),
-    (4, 'インサイダー取引防止研修', '', 1, 0, 2, @user_id, @user_id);
+    (1, 1, 'ビジネスマナーの基本', '', 1, 1, 2, @user_id, @user_id),
+    (2, 1, 'ロジカルシンキング研修', '', 1, 1, 2, @user_id, @user_id),
+    (3, 1, '情報セキュリティ研修', '', 0, 2, 2, @user_id, @user_id),
+    (4, 1, 'インサイダー取引防止研修', '', 0, 2, 1, @user_id, @user_id);
