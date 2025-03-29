26348913eae35077db0b4c6f07bf2251fc5274f0
revenue-hack <K.odeveloper10@gmail.com>
2025-03-29 12:34:13 +0900

    feat: make .zshrc.local in executing setup-zsh

--- shell/setup-zsh.sh
+++ shell/setup-zsh.sh
@@ -3,6 +3,7 @@ if [ ! -d "$HOME/.oh-my-zsh" ] ; then
   curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
   rm -rf ~/.zshrc
   ln -s ~/dotfiles/.zshrc ~/.zshrc
+  touch ~/dotfiles/.zshrc.local
   ln -s ~/dotfiles/.zshrc.local ~/.zshrc.local
 fi
 #exec $SHELL
