f20393def3e0238b4c2d195b5d3a0f24b795d451
revenue-hack <K.odeveloper10@gmail.com>
2025-04-02 12:23:53 +0900

    fix: change max tokens from max competition for avante

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -404,7 +404,7 @@ return {
             model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
             timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
             temperature = 0,
-            max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
             --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
           },
           behaviour = {
