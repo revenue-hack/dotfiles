797fccfbb936fb95bc40d231b935fc2e15c1aba9
revenue-hack <K.odeveloper10@gmail.com>
2025-06-05 10:04:30 +0900

    chore: change gpt model

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -423,7 +423,7 @@ return {
           provider = "openai",
           openai = {
             endpoint = "https://api.openai.com/v1",
-            model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+            model = "gpt-4.1", -- your desired model (or use gpt-4o, etc.)
             timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
             temperature = 0,
             max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
